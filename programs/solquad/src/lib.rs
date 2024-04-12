fd4dfcee4704d3b5742e7e6b5b4306e55de5f550
Goldfinger289 <166839846+Goldfinger289@users.noreply.github.com>
2024-04-12 22:39:09 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("2UamYZVPwsE2xdXMi1swQTcCLEak9wQgH5pGpXVXnAxs");
 
 #[program]
 pub mod solquad {
